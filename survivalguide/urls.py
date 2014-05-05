02aa418a6f9b0e01f9231bf554cce22dac987e9c
afs2015 <afs2015@afs2015-Aspire-5732Z.(none)>
2014-05-04 21:47:16 -0400

    First commit

---
+++ survivalguide/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+from .views import HomePageView, SignUpView, LoginView, LogOutView
+
+urlpatterns = patterns(
+    '',
+    # Examples:
+    # url(r'^$', 'survivalguide.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    
+    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', LogOutView.as_view(), name='logout'),
+    url(r'^accounts/register/$', SignUpView.as_view(), name='signup'),
+   
+    url(r'^talks/', include('talks.urls', namespace='talks')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
